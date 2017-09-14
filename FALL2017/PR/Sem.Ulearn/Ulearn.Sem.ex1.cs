799554da5c987f7a9264ef8abb367cfaad4fb3f5
Sizova_Darya_708 <daryasa1991@mail.ru>
2017-09-14 11:41:39 +0300

    first

---
+++ FALL2017/PR/Sem.Ulearn/Ulearn.Sem.ex1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem.Ulearn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+
+            b = b - a; // Значение 2
+            a = a + b; // Значение 7
+            b = a - b; // Значение 5
+            Console.WriteLine(a.ToString() + ", " + b.ToString());
+            
+           
+
+            
+        }
+    }
+}
---
+++ FALL2017/PR/Sem.Ulearn/Ulearn.Sem.ex4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulearn.Sem.ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int H = int.Parse(Console.ReadLine());
+            H = H * 30;
+            Console.WriteLine(H);
+        }
+    }
+}
