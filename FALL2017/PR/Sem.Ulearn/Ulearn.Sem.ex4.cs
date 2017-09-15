dd7f0b968eb9a1a2fe983cae1306ddbbaad4003a
Sizova_Darya_708 <daryasa1991@mail.ru>
2017-09-15 12:20:27 +0300

    Задания  с Ulearn

---
+++ FALL2017/PR/Sem.Ulearn/Ulearn.Sem.ex4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulearn.Sem.ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int N = int.Parse(Console.ReadLine()) - 1; // Числа, меньшие N
+           int Y = int.Parse(Console.ReadLine());
+           int X = int.Parse(Console.ReadLine());
+           int A = ((N / X) + (N / Y) - (N / (X * Y)));
+            Console.WriteLine(A);
+
+
+
+           
+
+
+
+        }
+    }
+}
---
+++ FALL2017/SEM/SEM2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = int.Parse(Console.ReadLine()); // Первый член арифметической прогрессии
+            int s = int.Parse(Console.ReadLine()); // Шаг прогрессии
+            int a = int.Parse(Console.ReadLine()); // Сам член прогрессии
+            int n = (a - f) / s + 1;
+
+            Console.WriteLine(n);
+        }
+    }
+}
