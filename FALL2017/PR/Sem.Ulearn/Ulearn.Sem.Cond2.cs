2a727e3352e2f0ad3b036572d3166eba994f7f02
Sizova_Darya_708 <daryasa1991@mail.ru>
2017-10-02 20:01:16 +0300

    Решение задач

--- FALL2017/PR/Sem.Ulearn/Ulearn.Sem.Cond2.cs
+++ FALL2017/PR/Sem.Ulearn/Ulearn.Sem.Cond2.cs
@@ -4,71 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Practise
-{
+namespace Ulearn.Sem.Cond2
+{   // Пролезет ли брус со сторонами x, y, z в отверстие со сторонами a, b, если его разрешается поворачивать на 90 градусов?
     class Program
     {
-        static void GetDistance(double ax, double ay, double bx, double by)
-        {
-            Math.Sqrt(Math.Abs((bx - ax) * (bx - ax) - (by - ay) * (by - ay)));
-        }
-        static void GetCentreOgSquareX(double ax, double bx)
-        {
-            (ax + bx) / 2;
-        }
-        static void GetCentreOgSquareY(double ay, double by)
-        {
-            (ay + by) / 2;
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Поочередно введите координаты  1 точки квадрата");
-            double x1 = double.Parse(Console.ReadLine());
-            double y1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Поочередно введите координаты  2 точки квадрата");
-            double x2 = double.Parse(Console.ReadLine());
-            double y2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Поочередно введите координаты  3 точки квадрата");
-            double x3 = double.Parse(Console.ReadLine());
-            double y3 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Поочередно введите координаты  4 точки квадрата");
-            double x4 = double.Parse(Console.ReadLine());
-            double y4 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите координаты центра окружности");
-            double x5 = double.Parse(Console.ReadLine());
-            double y5 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите радиус");
-            double r = double.Parse(Console.ReadLine());
-            double centreOfSquareX, centreOfSquareY;
+            Console.WriteLine("Введите ширину бруска");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину бруска");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите высоту бруска");
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ширину отверстия");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите высоту отверстия");
+            int b = int.Parse(Console.ReadLine());
+            
+            if (((x <= a || x <= b) && (y <= a || y <= b)) || ((x <= a || x <= b) && (z <= a || z <= b)) || ((y <= a || y <= b) && (z <= a || z <= b)))
+                Console.WriteLine("Брусок пройдет в отверстие");
+            else Console.WriteLine("Брусок не пройдет в отверстие");
 
-            double firstSide = (GetDistance(x1, y1, x2, y2));
-            double secondSide = (GetDistance(x1, y1, x3, y3));
-            double thirdSide = (GetDistance(x1, y1, x4, y4));
-
-            if ((firstSide > secondSide) && (firstSide > thirdSide))
-            {
-                centreOfSquareX = (GetCentreOgSquareX(x1, x2));
-                centreOfSquareY = (GetCentreOgSquareY(y1, y2));
-                sideOfSquare = secondSide / 2;
-            }
-            else if ((secondSide > firstSide) && (secondSide > thirdSide))
-            {
-                centreOfSquareX = (GetCentreOgSquareX(x1, x3));
-                centreOfSquareY = (GetCentreOgSquareY(y1, y3));
-                sideOfSquare = firstSide / 2;
-            }
-            else
-            {
-                centreOfSquareX = (GetCentreOgSquareX(x1, x4));
-                centreOfSquareY = (GetCentreOgSquareY(y1, y4));
-                sideOfSquare = firstSide / 2;
-            }
-                 Console.WriteLine((centreOfSquareX == x5) && (centreOfSquareY = y5) && (sideOfSquare == r));
-
-
-
-
-
+           
         }
     }
 }
---
+++ FALL2017/SEM/1/SEM6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM6
+{
+    class Program
+    {   // Найти последнюю цифру A^B (1<=A,B<=10000)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, возводимое в степень, где оно меньше или равно единице");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень числа, где оно меньше или равно 10000");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((Math.Pow(a, b)) % 10); 
+
+        }
+    }
+}
