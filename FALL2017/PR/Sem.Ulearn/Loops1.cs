fe3ee4b8db16f9c903401ab94c38b5146bf5894d
Sizova_Darya_708 <daryasa1991@mail.ru>
2017-10-05 13:07:53 +0300

    Решения семинарских заданий

---
+++ FALL2017/PR/Sem.Ulearn/Loops1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops1
+{
+    class Program
+    { // Дано целое неотрицательное число N. Найти число, составленное теми же десятичными цифрами, что и N, но в обратном порядке. Запрещено использовать массивы.
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            char[] b = N.ToCharArray();
+            Array.Reverse(b);
+            N = new string(b);
+            Console.WriteLine(N);
+        }
+    }
+}
---
+++ FALL2017/PR/Sem.Ulearn/Loops2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops2
+{
+    class Program
+    { // Найти количество трехзначных натуральных чисел, сумма цифр которых равна N. 
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int J = 0; J < 10; J++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        if (i + J + k == N) count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
