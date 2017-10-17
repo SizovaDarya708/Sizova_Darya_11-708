9c5cf4487b129aedf37315139e92746867af1a68
Sizova_Darya_708 <daryasa1991@mail.ru>
2017-10-17 18:05:28 +0300

    [abs] Привела в порядок иерархию папок

---
+++ FALL2017/PR/Sem.Ulearn/Семинарские задания. Циклы/Loops4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops4
+{ //В массиве чисел найдите самый длинный подмассив из одинаковых чисел.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int count = 1;
+            int number;
+            int lastNumber; 
+            int maxOfNumbers = 0;
+
+            Console.WriteLine("Введите последнее число последовательности(массива)");
+            lastNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                if (number == lastNumber) count++;
+                else
+                {
+                    lastNumber = number;
+                    maxOfNumbers = Math.Max(count, maxOfNumbers);
+                    count = 1;
+                }
+
+            }
+
+            Console.WriteLine(Math.Max(maxOfNumbers, count));
+        }
+    }
+}
---
+++ FALL2017/PR/Sem.Ulearn/Семинарские задания. Циклы/Loops5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops5
+{
+    class Program
+    { // Дана строка из символов '(' и ')'.Определить, является ли она корректным скобочным выражением. Определить максимальную глубину вложенности скобок.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку скобок");
+            var str = Console.ReadLine();
+
+            int nesting = 0,  // начальная вложенность
+                maxNesting = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(')
+                    nesting++;
+                if (nesting > maxNesting)
+                    maxNesting = nesting;
+                if (str[i] == ')')
+                    nesting--;
+            }
+            if (nesting == 0)
+                Console.WriteLine("Максимальная вложенность скобок" + maxNesting);
+            else Console.WriteLine("Некорректный ввод строки");
+
+        }
+    }
+}
