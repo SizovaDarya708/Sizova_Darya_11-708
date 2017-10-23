a7a44401fe107df0e6b713b2589b69b598cde474
Sizova_Darya_708 <daryasa1991@mail.ru>
2017-10-23 14:52:13 +0300

    [abs]Обновление структуры папок

---
+++ FALL2017/PR/Sem.Ulearn/Семинарские задания. Ошибки/Ulearn.Sem.ex10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem.Ulearn.ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 0; // Счетчик чисел делищихся на 3 или 5
+
+            for (int i=0; i<1000; i++)
+            { if (i % 3 == 0 || i % 5 == 0)
+                { amount += i; }
+            }
+            Console.WriteLine(amount);
+
+        }
+    }
+}
