7f5d8dc8d0a5d0bc49cd4047c92e08d3cbb06691
Your Name <you@example.com>
2018-05-28 13:28:11 +0300

    Вторая контрольная

---
+++ SPRING2018/MISC/Вторая контрольная/ex1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ex._1
+{
+    /*По адресу https://jsonplaceholder.typicode.com/photos получить список картинок в виде json.
+     * Реализовать скачивание картинок по адресу thumbnailUrl. 
+     * Использовать максимально все технологии, которые вы знаете: 
+     * многопоточное или асинхронное программирование, linq и т.д. 
+     * Парсер json можно использовать библиотечный. */
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+             var listOfData = new List<object> { };
+
+            /* string url = "https://jsonplaceholder.typicode.com/photos";
+             using (WebClient client = new WebClient())
+             {
+                 client.DownloadFile(url, AppDomain.CurrentDomain.BaseDirectory + "test.jpg");
+                 listOfData.Add(url);
+             }*/
+
+            WebClient webClient = new WebClient();
+            WebClient url = new WebClient();
+
+            var data = url.DownloadString("https://jsonplaceholder.typicode.com/photos");
+            JObject o = JObject.Parse(data);
+
+            var json = JsonConvert.SerializeObject(listOfData, Formatting.Indented);
+      
+            foreach (var field in listOfData)
+            {
+                listOfData.Add(field.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SPRING2018/MISC/Вторая контрольная/ex2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web.Script.Serialization;
+
+namespace ex._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //нашла в интернете этот тип для простой работы
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            // Лист obj, в котором хранятся данные
+            String dataString;
+            int a = 0; //  количество
+            List<object> listOfData = new List<object>() { new MyClass() };
+            List<char> symbols = new List<char> { 'b', 'c', 'd', 'f', 'j', 'k', 'l',
+                'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'
+            };
+            
+            
+            for (int i = 0; i < listOfData.Count; i++)
+            {
+                // получаем тип 
+                Type type = listOfData[i].GetType();
+                // информацию о методе
+                MethodInfo[] methodInfo = type.GetMethods();
+                // работа в цикле
+                for (int j = 0; j < methodInfo.Length; j++)
+                {
+                    a = 0;
+                    // Для больших и маленьких букв
+                    dataString = methodInfo[j].Name.ToLower();
+
+                    for (int n = 0; n < dataString.Length; n++)
+                    {
+                        for (int m = 0; m < symbols.Count; m++)
+                        {
+                            if (dataString[n] == symbols[m])
+                                a++;
+                        }
+                    }
+                    if (a % 2 != 1)
+                    {
+                        Console.WriteLine(methodInfo[j].Name.ToString());
+                        // преобразование в строку
+                        json.Serialize(methodInfo[j].Name.ToString());
+                    }
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+    // "какой-то" класс для работы с ним
+    public class MyClass
+    {
+        public int Method(int argument)
+        {
+            return argument + 1;
+        }
+        public double Property { get; set; }
+    }
+}
+
